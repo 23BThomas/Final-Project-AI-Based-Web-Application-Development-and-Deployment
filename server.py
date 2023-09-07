d81420b866648a4f4d331e5b4c8b375b93c1f1ab
23BThomas <136824128+23BThomas@users.noreply.github.com>
2023-09-07 13:48:27 +0400

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 # Import Flask, render_template, request from the flask framework package
 from flask import Flask, render_template, request
 
-# Import the sentiment_analyzer function from the package created:
+# Import the emotion_detector function from the package created:
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
